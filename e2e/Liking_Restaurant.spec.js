23e031b345b5c0c78e8ec6b269049bf12bde450d
abgvin <muhamadapri10@gmail.com>
2020-12-11 02:45:06 +0700

    Unliked one restaurant

--- e2e/Liking_Restaurant.spec.js
+++ e2e/Liking_Restaurant.spec.js
@@ -31,3 +31,33 @@ Scenario('liking one Restaurant', async({ I }) => {
 
   assert.strictEqual(firstRestaurantTitle, likedRestaurantTitle);
 });
+
+Scenario('Unliked one restaurant', async({ I }) => {
+  I.see('Your Favorite List is Empty', '#error-text');
+
+  I.amOnPage('/');
+
+  I.seeElement('.box .text h2 a');
+
+  const firstRestaurant = locate('.box .text h2 a').first();
+  const firstRestaurantTitle = await I.grabTextFrom(firstRestaurant);
+  I.click(firstRestaurantTitle);
+
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+
+  I.amOnPage('/#/like');
+  I.seeElement('.box .text h2');
+  const likedRestaurantTitle = await I.grabTextFrom('.box .text h2');
+
+  assert.strictEqual(firstRestaurantTitle, likedRestaurantTitle);
+
+  I.click(likedRestaurantTitle);
+
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+
+  I.amOnPage('/#/like');
+
+  I.see('Your Favorite List is Empty', '#error-text');
+});
